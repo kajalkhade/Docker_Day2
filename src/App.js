87c30be35bc7802076e532fd56a5c1403cfd2984
Ubuntu <ubuntu@ip-172-31-39-240.ap-south-1.compute.internal>
2026-03-05 05:47:37 +0000

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  // Example: fetch from a backend server if you have one
+  useEffect(() => {
+    fetch("http://localhost:5000") // Make sure your Node.js server is running here
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch(() => setMessage("Could not fetch data"));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React App</h1>
+      <p>{message || "No data yet..."}</p>
     </div>
   );
 }
